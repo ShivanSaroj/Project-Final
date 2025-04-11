d075933375404ccad0f30934bb88725797e63324
Shivani Saroj <156885424+ShivanSaroj@users.noreply.github.com>
2025-04-12 02:45:19 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,10 +19,16 @@ function Login() {
     const handleLoginSuccess = (userData)=>{
         console.log("the logged in userdata: ",userData);
         login(userData);
-        if(!userData.isMfaActive){
-            navigate("/setup-2fa");
-        }else{
-            navigate("/verify-2fa");
+        // if(!userData.isMfaActive){
+        //     navigate("/setup-2fa");
+        // }else{
+        //     navigate("/verify-2fa");
+        // }
+        if(userData){
+            navigate("/");
+        }
+        else{
+            navigate("/login");
         }
     };
 
